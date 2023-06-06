be5397c478bed64c2da570547f61ad0de3a4ccda
harijo123 <hariijack.97@gmail.com>
2023-06-06 13:20:49 +0530

    hari

--- src/org/test/B.java
+++ src/org/test/B.java
@@ -1,17 +1,23 @@
 package org.test;
 
-public class B  {
+public class B {
 
 	private void c1() {
 		System.out.println("hari");
 	}
+
 	private void c2() {
 		System.out.println("priya");
 	}
 
-private void c3() {
-	System.out.println("agni");
-}
+	private void c3() {
+		System.out.println("agni");
+	}
+
+	private void c4() {
+		System.out.println("mithran");
+	}
+
 	public static void main(String[] args) {
 		B b = new B();
 		b.c1();
