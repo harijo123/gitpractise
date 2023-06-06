3b27f69911a51a0f529819bc910de02bf6ee4ef3
harijo123 <hariijack.97@gmail.com>
2023-06-06 12:42:12 +0530

    task

--- src/org/test/B.java
+++ src/org/test/B.java
@@ -3,11 +3,15 @@ package org.test;
 public class B  {
 
 	private void c1() {
-
+		System.out.println("hari");
+	}
+	private void c2() {
+		System.out.println("music king");
 	}
 	public static void main(String[] args) {
 		B b = new B();
 		b.c1();
+		b.c2();
 	}
 
 }
